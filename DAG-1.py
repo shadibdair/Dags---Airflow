344953cb4f2a175ad91b91a26cf1a540bbe4f5d0
bdairshadi <bdairshadi@gmail.com>
2022-01-23 11:10:21 +0200

    BUG

--- DAG-1.py
+++ DAG-1.py
@@ -3,3 +3,22 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 # Because we need to define tasks and in order to defined we will be using operator!
+from airflow.operator.dummy_operator import DummyOperator
+
+# Step -2
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(22, 1, 23),
+    'retries': 0
+}
+
+# Step -3
+# Instantiate the DAG
+dag = DAG(dag_id='DAG-1', default_args=default_args, catchup=False, schedule_interval='@once')
+
+
+# Define tasks
+
+# Define dependecies
