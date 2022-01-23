74bdaef38a2262b39a29028d53c3e1e01ffec3b6
bdairshadi <bdairshadi@gmail.com>
2022-01-23 11:04:33 +0200

    Define arguments + Instantiaite the DAG

--- DAG-1.py
+++ DAG-1.py
@@ -2,4 +2,4 @@
 # Modules
 from airflow import DAG
 from datetime import datetime, timedelta
-# Because we need to define tasks and in order to defined we will be using operator
+# Because we need to define tasks and in order to defined we will be using operator!
